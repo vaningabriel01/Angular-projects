2ffd9d818196dca01c432061c430b57e0ea15dcb
Gabriel Vanin <vaningabriel01@gmail.com>
2026-04-26 18:53:31 -0300

    Adicionando a page home onde é apresentado tres cards onde cada um mostra uma breve parte do conteudo e ao clicar te leva para o conteudo completo que ainda sera implementado

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,9 +1,45 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { NgFor } from '@angular/common';
+
+// Estrutura de dados de um post
+interface Post {
+  id: number;
+  titulo: string;
+  resumo: string;
+  data: string;
+  imagem: string; // URL simples como string normal
+}
 
 @Component({
   selector: 'app-home',
-  imports: [],
   templateUrl: './home.html',
-  styleUrl: './home.css',
+  styleUrls: ['./home.css'],
+  imports: [RouterLink, NgFor]
 })
-export class Home {}
+export class Home {
+
+  posts: Post[] = [
+    {
+      id: 1,
+      titulo: 'Exploração no Pré-Sal da Bacia de Santos',
+      resumo: 'A PetroEnergy iniciou uma nova fase de exploração na Bacia de Santos, com estimativa de 2 bilhões de barris de petróleo em reservas.',
+      data: '10 Abr 2025',
+      imagem: 'https://nossaenergia.petrobras.com.br/o/adaptive-media/image/41911941/Preview-1000x0/nossa-energia-bacia-de-santos-destaque.jpg?t=1762346296000'
+    },
+    {
+      id: 2,
+      titulo: 'Nova Plataforma de Extração no Golfo do México',
+      resumo: 'A instalação da plataforma P-78 marca a expansão internacional da empresa, com operações previstas para início em 2026.',
+      data: '22 Mar 2025',
+      imagem: 'https://cdn.oantagonista.com/uploads/2026/01/plataforma-de-petroleo_1767816931196-1024x576.jpg'
+    },
+    {
+      id: 3,
+      titulo: 'Projeto Gás Verde: Energia com Menor Pegada de Carbono',
+      resumo: 'O projeto visa reduzir as emissões durante a extração de gás natural, tornando o processo mais eficiente e sustentável.',
+      data: '05 Mar 2025',
+      imagem: 'https://abiogas.com.br/wp-content/uploads/2024/12/63cecc902a95285df318e89c_AF-GAS_VERDE_LOGOS-RGB-1-p-500.jpg'
+    }
+  ];
+}
