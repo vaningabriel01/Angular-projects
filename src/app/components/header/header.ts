e12eeb98cdcff6e6e3f31db6ae84e23d90c83a35
Gabriel Vanin <vaningabriel01@gmail.com>
2026-04-26 16:09:14 -0300

    Foi implementado o modulo do header com os arquivos: header.html, header.css, header.ts

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  imports: [],
   templateUrl: './header.html',
-  styleUrl: './header.css',
+  styleUrls: ['./header.css']
 })
 export class Header {}
